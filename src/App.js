64034297f24ac6f0a0e301f025b9efbe68275e14
ASUS <Yehor Kholodiuk>
2022-06-07 21:07:23 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import {useState} from "react";
 function App() {
   const [cards, setCards] = useState([])
+  const [formTaskName, setFormTaskName]=useState([])
   const loadTasks = () => {
     console.log('load starts');
     axios ({
@@ -13,6 +14,21 @@ function App() {
     setCards(res.data)
     })
   };
+
+
+  const createTask = () => {
+      axios ({
+          url:'https://pasv-kanban.herokuapp.com/card',
+          method:'POST',
+          data: {
+              name: formTaskName,
+              'status':'todo',
+              'priority': 1
+          }
+      }).then (()=> {loadTasks()
+      setFormTaskName('')
+      })
+  }
   return (
     <div>
       <button onClick={loadTasks}>Load Tasks</button>
@@ -20,6 +36,9 @@ function App() {
 
         {cards.map(el => <li key={el.id}>{el.name}</li>)}
       </ul>
+      <hr/>
+      <input value={formTaskName} onChange={e => setFormTaskName(e.target.value)}/>
+        <button onClick={createTask}>Create Task</button>
     </div>
   );
 }
